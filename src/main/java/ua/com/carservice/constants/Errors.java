5ea79a401e15e96a45495d3d1f7ced850a9ebbd0
Vitaliy <boyko1997vit@gmail.com>
2020-10-18 19:08:27 +0300

    Add Error constants in CarServiceImpl

---
+++ src/main/java/ua/com/carservice/constants/Errors.java
@@ -0,0 +1,9 @@
+package ua.com.carservice.constants;
+
+public final class Errors {
+
+    public static final String NOT_FOUND_YEAR="The cars has not been found since that year:";
+    public static final String FIELD_USERID_IS_EMPTY="=Enter user Id";
+    public static final String NOT_FOUND_INFO="Not found information";
+
+}
